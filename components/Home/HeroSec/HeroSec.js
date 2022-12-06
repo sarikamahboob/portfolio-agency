5eacbb1798b982f455d4440d57488321aa00c560
sarikamahboob <sarika.mahboob1234@gmail.com>
2022-12-07 01:38:00 +0600

    first commit

---
+++ components/Home/HeroSec/HeroSec.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./HeroSec.module.scss";
+
+const HeroSec = () => {
+    return (
+        <div>
+            <h1 className={styles.red} >Lionel Messi</h1>
+        </div>
+    );
+};
+
+export default HeroSec;
